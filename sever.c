19228050bc48bbdfd31c803aacb97e5b4b3ac717
psp10 <psp10@asb9700u-g02.csil.sfu.ca>
2024-02-13 17:00:09 -0800

    hi

---
+++ sever.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "list.h"
+
+#define PORT 6001
+ 
+
+int main()
+{
+
+}
