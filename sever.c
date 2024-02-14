3b43985d2c20446dba07aa1cb634f40b6ef7f8da
psp10 <psp10@asb9700u-g02.csil.sfu.ca>
2024-02-13 17:33:48 -0800

    starting

--- sever.c
+++ sever.c
@@ -8,9 +8,42 @@
 #include "list.h"
 
 #define PORT 6001
- 
+#define MSG_MAX_Len 1024;
 
 int main()
 {
+    struct sockaddr_in sin;
+    memset(&sin,0,sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_port = hort(PORT);
+
+    // create socket PF can be used 
+    int socketDes = socket(AF_INET,SOCK_DGRAM,0);
+
+    bind (socketDes, (struct sockaddr*) &sin, sizeof(sin));
+
+    while (1)
+    {
+        struct sockaddr_in sinRemote;
+        unsigned int sin_len = sizeof(sinRemote);
+        char messageRX[MSG_MAX_Len];
+        int byteRx = recvfrom(socketDes, messageRX, MSG_MAX_Len, 0, (struct sockaddr*)&sinRemote,&sin_len);
+
+        int terminateIdx = (byteRx < MSG_MAX_Len) ? byteRx : MSG_MAX_Len-1;
+        messageRX[terminateIdx] = 0;
+        printf("Message received (%d bytes): \n\n'%s'\n", byteRx, messageRX);
+
+        int incMe = atoi(messageRX);
+
+        char messageTx[MSG_MAX_Len];
+        sprintf(messageTx, "Math: %d + 1 = %d\n", incMe, incMe+1);
+
+        sin_len = sizeof(sinRemote);
+        sendto (socketDes, messageTx,strlen(messageTx),0,(struct sockaddr *)&sinRemote, sin_len);
+
+
+
+    }
 
 }
